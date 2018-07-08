1eae7592dfce2f60088f0f62686be9e628f0ccef
Joana Palero <joana.palero24@gmail.com>
2018-07-08 15:57:06 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,8 +2,8 @@ class Backer
   attr_accessor :name, :backed_projects
   
   def initialize(name)
-    @name=name
-    @backed_projects=[]
+    @name = name
+    @backed_projects = []
   end
   
   def back_project(project)
